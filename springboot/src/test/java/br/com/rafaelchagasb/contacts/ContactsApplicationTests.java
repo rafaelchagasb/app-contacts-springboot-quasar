7d040cb9633b2d910e8baf2db93382f7b9e21ac1
Rafael Chagas <rafa15chagas@gmail.com>
2020-06-17 23:22:06 -0300

    add structure folder

---
+++ springboot/src/test/java/br/com/rafaelchagasb/contacts/ContactsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelchagasb.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
